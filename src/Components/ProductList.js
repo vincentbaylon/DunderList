dc5463206de0db59d817440b569173716f83e998
Claire <clairelogan16@gmail.com>
2021-08-03 09:28:53 -0700

    Built out Shop product list component and created placeholder filter and search components

---
+++ src/Components/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+function ProductList( { products }) {
+
+    return (
+        <div className="productListContainer">
+            <ul>
+            {products.map(product => {return (<ProductCard product={product} key={product.id}/>) })}
+            </ul>
+        </div>
+    )
+}
+
+export default ProductList
