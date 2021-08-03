6d5e28d781ac6a9cca2e2b560eb13099ca0f2205
vincentbaylon <vincentbaylon@gmail.com>
2021-08-03 14:38:02 -0700

    Filter changes

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -4,13 +4,13 @@ import ProductList from './ProductList'
 import Search from './Search'
 import Sort from './Sort'
 
-function Shop({ products, search, setSearch, setFilter, addToCart, removeFromCart, setSort }) { 
+function Shop({ products, search, setSearch, setFilter, addToCart, removeFromCart, setSort, filter }) { 
 
     return (
         <div className="shopContainer">
             <div>
                 <Sort setSort={setSort} />
-                <Filters setFilter={setFilter} />
+                <Filters filter={filter} setFilter={setFilter} />
                 <Search seach={search} setSearch={setSearch}/>
                 <ProductList products={products} addToCart={addToCart} removeFromCart={removeFromCart}/>
             </div>
