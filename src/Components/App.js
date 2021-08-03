1a30d3f7cea4cb4ac433620891e5993ac47055bd
Claire <clairelogan16@gmail.com>
2021-08-03 08:51:17 -0700

    Added product state & fetch to db.json

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Header from './Header'
 import Shop from './Shop'
 import Sell from './Sell'
@@ -6,6 +7,16 @@ import About from './About'
 import Cart from './Cart'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/products`)
+    .then(res => res.json())
+    .then(setProducts)
+  }, [])
+
+console.log(products)
+
   return (
     <div>
      <Header />
