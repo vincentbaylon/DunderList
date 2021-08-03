c0e7ac0a3215c6b13c4d6f43047b0b8479401022
Claire <clairelogan16@gmail.com>
2021-08-02 17:08:40 -0700

    Added Header and NavLinks

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const headerStyle ={
+    backgroundColor: "#0D1F2D",
+    width: "100%",
+    color: "white", 
+    paddingTop: '10px',
+    paddingLeft: '10px'
+}
+
+const navStyle = {
+    textAlign: "right",
+    paddingBottom: "10px",
+    paddingRight: "10px"
+}
+
+const linkStyles = {
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    color: "white",
+    textDecoration: "none"
+  }
+
+const selectedLink ={
+    fontWeight: "bold"
+}
+
 
 function Header() {
     return (
-        <header>
-            <div id="logo">
+        <header style={headerStyle}>
+            <div id="logo" >
             <h1>DunderList</h1>
             </div>
-            <nav>
-                
-
-            </nav>
+            <div id="navBar" style={navStyle}>
+                <NavLink exact to="/" style={linkStyles} activeStyle={selectedLink}>Shop</NavLink>
+                <NavLink to="/sell" style={linkStyles} activeStyle={selectedLink}>Sell</NavLink>
+                <NavLink to="/about" style={linkStyles} activeStyle={selectedLink}>About</NavLink>
+                <NavLink to="/cart" style={linkStyles} activeStyle={selectedLink}>Cart</NavLink>
+            </div>
         </header>
     )
 }
