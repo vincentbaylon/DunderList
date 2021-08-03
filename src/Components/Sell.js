5eb661f1afde9856174c299f23e6f1d37594c869
vincentbaylon <vincentbaylon@gmail.com>
2021-08-02 17:47:12 -0700

    Added inputs and dropdowns for sell form

--- src/Components/Sell.js
+++ src/Components/Sell.js
@@ -3,7 +3,28 @@ import React from 'react'
 function Sell() {
     return (
         <div>
-            Sell
+            <form>
+                <input type="text" name="name" placeholder="Product Name" />
+                <input type="text" name="imageUrl" placeholder="Image Url" />
+                <input type="number" step="0.01" name="price" placeholder="Price" />
+                <select name="category" id="category" aria-label="Category">
+                    <option value="foodDrink">Food N Drink</option>
+                    <option value="home">Home</option>
+                    <option value="apparel">Apparel</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="misc">Miscellaneous</option>
+                </select>
+                <select name="sellers" id="sellers" aria-label="Sellers">
+                    <option value="jim">Jim Halpert</option>
+                    <option value="pam">Pam Beasley</option>
+                    <option value="dwight">Dwight Schrute</option>
+                    <option value="michael">Michael Scott</option>
+                    <option value="jan">Jan Levinson</option>
+                </select>
+
+                <input type="submit" value="Submit" />
+            </form>
+            
         </div>
     )
 }
