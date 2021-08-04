1c08bcfff70a28b5903d236e89dfddee82ce05ec
Claire <clairelogan16@gmail.com>
2021-08-04 09:24:15 -0700

    Added styling to Cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import CartCard from './CartCard'
 
 function Cart({ cart, products, removeFromCart }) {
+
     const filterProduct = products.filter((eachProduct) => {
         return cart.includes(eachProduct.id)
     })
@@ -10,9 +12,22 @@ function Cart({ cart, products, removeFromCart }) {
         return <CartCard key={eachFiltered.id} product={eachFiltered} removeFromCart={removeFromCart} />
     })
 
+    const totalCost = Number.parseFloat(filterProduct.reduce((accum, product)=> {
+        return product.price + accum}, 0)).toFixed(2)
+
     return (
-        <div>
-            {displayProduct}
+        <div className="ui center aligned container">
+            <br></br>
+            <h2>Your Cart</h2>
+            <br></br>
+            <div className="ui one column center aligned grid">
+                {displayProduct}
+            </div>
+            <br></br>
+            <div className="ui basic segment">
+                <h3>Total:&nbsp;&nbsp;&nbsp;&nbsp; ${totalCost}</h3>
+            </div>
+          
         </div>
     )
 }
