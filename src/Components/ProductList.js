5283850199fa88de90ec6837a00f0810f9b81c32
Claire <clairelogan16@gmail.com>
2021-08-04 09:54:05 -0700

    minor CSS tweaks

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -8,7 +8,7 @@ function ProductList( { products, addToCart, removeFromCart}) {
     }
 
     return (
-        <div className="ui three column grid" style={productStyle}>
+        <div className="ui column grid" style={productStyle}>
             {products.map(product => {return (<ProductCard product={product} key={product.id} addToCart={addToCart} removeFromCart={removeFromCart}/>) })}
         </div>
     )
